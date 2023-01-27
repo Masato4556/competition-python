79c2be20510df52400f287fc0c3bc449fce219e5
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-01-27 16:46:37 +0900

    20230127

--- ABC276/E/answer.py
+++ ABC276/E/answer.py
@@ -1,52 +1,78 @@
-# TODO union findで解く方法を試す
-# Sの上下左右のマスのうちの２つが、同じグループに属していれば”Yes”
-
-from queue import Queue
 from  itertools import combinations
+from collections import defaultdict
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.par = [-1] * n
+        self.rank = [0] * n
+        self.siz = [1] * n
+
+    def root(self, x):
+        try:
+            if self.par[x] == -1: return x
+        except IndexError as e:
+            print(x, e)
+
+        else:
+          self.par[x] = self.root(self.par[x]) # 経路圧縮
+          return self.par[x]
+
+    def issame(self, x, y):
+        return self.root(x) == self.root(y)
+
+    def unite(self, x, y):
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry: return False
+        # union by rank
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.par[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        self.siz[rx] += self.siz[ry]
+        return True
+
 
 h, w = map(int, input().split())
 C = [input() for _ in range(h)]
 
-G = [[[] for _ in range(w)] for _ in range(h)]
+def calc_index(x, y):
+    return y*w + x
+
+def is_valid_pos(x, y):
+    if x < 0 or w <= x: return False
+    if y < 0 or h <= y: return False
+    return True
+
+uf = UnionFind(h * w)
+
 base_pos = (0, 0)
 offsets = [(0, 1), (0, -1), (1, 0), (-1,0)]
 for y in range(h):
     for x in range(w):
         if C[y][x] == "#": continue
-        if C[y][x] == "S": base_pos = (x, y)
+        if C[y][x] == "S": 
+            base_pos = (x, y)
+            continue
         for offset_x, offset_y in offsets:
             next_x = x + offset_x
             next_y = y + offset_y
-            if next_x < 0 or w <= next_x: continue
-            if next_y < 0 or h <= next_y: continue
-            if C[next_y][next_x] == "#": continue
-            G[y][x].append((next_x, next_y))
+            if not is_valid_pos(next_x, next_y): continue
+            if C[next_y][next_x] in ("#", "S"): continue
+            uf.unite(calc_index(x, y), calc_index(next_x, next_y))
 
 result = "No"
 for s_offset, e_offset in combinations(offsets, 2):
     s_x, s_y = base_pos[0]+s_offset[0], base_pos[1]+s_offset[1]
     e_x, e_y = base_pos[0]+e_offset[0], base_pos[1]+e_offset[1]
 
-    if result == "Yes": break
-    if C[s_y][s_x] == "#": continue
-    if C[e_y][e_x] == "#": continue
+    if not is_valid_pos(s_x, s_y): continue
+    if not is_valid_pos(e_x, e_y): continue
 
-    que = Queue()
-    que.put((s_x, s_y))
-    seen = [[False for _ in range(w)] for _ in range(h)]
-    seen[base_pos[1]][base_pos[0]] = True
-    
-    while not que.empty():
-        x, y = que.get()
-
-        seen[y][x] = True
-
-        for next_x, next_y in G[y][x]:
-            if seen[next_y][next_x]: continue
-            if next_x == e_x and next_y == e_y:
-                result = "Yes"
-                break 
-            que.put((next_x, next_y))
-
+    if uf.issame(calc_index(s_x, s_y), calc_index(e_x, e_y)):
+        result = "Yes"
+        break
 
 print(result)
