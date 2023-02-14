18f829ba29a40ce00df75e76200ebfa2fae61821
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-02-14 17:41:21 +0900

    コンテストごとにフォルダを生成するように変更

--- make_folder.py
+++ make_folder.py
@@ -1,6 +1,26 @@
+import os
 import shutil
 import datetime
+import argparse
 
+def make_contest_folder(name):
+    dir  = "./{}".format(name)
+    os.mkdir(dir)
+
+def make_question_folder(contest, questions):
+    contest_dir  = "./{}".format(contest)
+    for q in questions:
+        shutil.copytree("./template", contest_dir + "/{}".format(q))
+
+
+parser = argparse.ArgumentParser(description='フォルダの作成') 
 dt_now = datetime.datetime.now()
-dir_name  = "./{}".format(dt_now.strftime('%Y%m%d%H%M%S'))
-shutil.copytree("./template", dir_name)
+parser.add_argument('-c', '--contest', default=dt_now.strftime('%Y%m%d%H%M%S'))
+parser.add_argument('-q', '--questions', nargs='*', default=["A", "B", "C", "D", "E", "F", "G", "H-Ex"])
+
+args = parser.parse_args()
+make_contest_folder(args.contest)
+make_question_folder(args.contest, args.questions)
+
+
+
