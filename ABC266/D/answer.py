e907213be0c2298b035efac6185b338a7e805035
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-03-22 17:58:01 +0900

    20230322

--- ABC266/D/answer.py
+++ ABC266/D/answer.py
@@ -2,9 +2,19 @@ N = int(input())
 
 dp = [[-1] * 5 for _ in range(N+1)]
 dp[0][0] = 0
-for _ in range(N):
-    T, X, A = map(int, input().split())
-    for i in range(5):
-        
 
-print(dp)
+prev_T = 0
+for i in range(N):
+    T, X, A = map(int, input().split())
+    diff_T = T - prev_T
+    prev_T = T
+
+    for j in range(5):
+        
+        if dp[i][j] == -1: continue
+        for k in range(max(0, j - diff_T), min(5, j + diff_T+1)):
+            v = dp[i][j] if k != X else dp[i][j] + A
+            dp[i+1][k] = max(dp[i+1][k], v)
+            
+
+print(max(dp[-1]))
--- ABC268/D/answer.py
+++ ABC268/D/answer.py
@@ -1,6 +1,54 @@
+from collections import defaultdict
 
-n = int(input())
-m = int(input())
-print(n)
-print(m)
+n, m = map(int, input().split())
+S = []
+for _ in range(n):
+    S.append(input())
 
+# TODO キャッシュ
+def f(s):
+    if s in S:
+        return S.index(s)
+    
+    return -
+    
+# TODO おける_の数を考える
+
+T = defaultdict(set)
+for _ in range(n):
+    t = input()
+
+    i = 0
+    flg = 1
+    h = []
+    h2 = []
+    for j in range(len(t)):
+        if flg == 1 and t[j] == "_":
+            print(t[i:j])
+            a = f(t[i:j])
+            if a == -1: break
+            h.append(a)
+            i = j
+            flg *= -1
+        
+        if flg == -1 and t[j] != "_":
+            h2.append(j-i)
+            print(t[i:j])
+            i = j
+            flg *= -1
+            
+        j += 1
+
+    a = f(t[i:])
+    if a != -1: 
+        h.append(a)
+
+    if len(h) != n: break
+    print(h, h2)
+            
+
+
+    T[tuple(h)].add(tuple(h2))
+
+
+print(T)
