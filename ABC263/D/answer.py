20cce696acd65dd463c5d080da1e204f9dd4e394
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-03-27 17:28:37 +0900

    ABC263D

--- ABC263/D/answer.py
+++ ABC263/D/answer.py
@@ -1,20 +1,62 @@
+# testcase 6　で落ちてしまう
 
-# string
-s = input()
+def f(arr):
+    n = len(arr)
+    res = [0] * (n+1)
+    for i in range(1, n+1):
+        res[i] = res[i-1] + arr[i-1]
+    return res
 
-# int
-n = int(input())
 
-# list
-a = list(map(int, input().split()))
+N, L, R = map(int, input().split())
+A = list(map(int, input().split()))
 
-# map
-n, m = map(int, input().split())
+if L > R:
+    L, R = R, L
+    A.reverse()
 
-# 有向グラフ
-G = [set() for _ in range(n)]
-degs = [0 for _ in range(n)]
-for _ in range(m):
-    u, v = map(lambda x: int(x)-1, input().split())
-    G[u].add(v)
-    degs[u] += 1
+S = f(A)
+A.reverse()
+S_rev = f(A)
+
+
+ll = [0]
+rr = [0]
+
+m = 0
+prev = -1
+for i in range(1, N+1):
+    if L*i - S[i] < m:
+        m = L*i - S[i]
+        prev = i
+    else:
+        if prev == -1: continue 
+        ll.append(i-1)
+        prev = -1
+if prev != -1: 
+    ll.append(i)
+
+
+m = 0
+prev = -1
+for i in range(1, N+1):
+    if R*i - S_rev[i] < m:
+        m = R*i - S_rev[i]
+        prev = i
+    else:
+        if prev == -1: continue 
+        rr.append(i-1)
+        prev = -1
+if prev != -1: 
+    rr.append(i)
+
+
+ans = sum(A)
+rr.reverse()
+for l in ll:
+    for r in rr:
+        if l > (N-r): continue
+        ans = min(ans, S_rev[-1] - S_rev[r] - S[l] + r*R + l*L)
+        break
+
+print(ans)
--- ABC263/D/lab.py
+++ ABC263/D/lab.py
@@ -1,1 +1,14 @@
 # テストデータ生成など、回答とは関係のないコードを実行するファイル
+from itertools import cycle
+
+input_path = "./testcases/input/7.txt"
+N = 2 * (10**5)
+L = 0
+R = 0
+
+with open(input_path, mode='w') as f:
+    s = f.write(f'{N} {L} {R}\n')
+    for i in cycle(range(2)):
+        s = f.write(f"{i} ")
+    s = f.write("\n")
+
