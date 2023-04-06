072a9f462e84891be7ed4b89bfcf6dfd09b9c392
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-04-06 09:51:31 +0900

    ABC266E

--- ABC266/E/answer.py
+++ ABC266/E/answer.py
@@ -1,6 +1,22 @@
+import sys
+from functools import lru_cache
 
-n = int(input())
-m = int(input())
-print(n)
-print(m)
+sys.setrecursionlimit(10**6)
+
+N = int(input())
+
+@lru_cache(None)
+def dfs(n, v):
+    if n >= N: 
+        return v
+
+    vv = 0
+    for i in range(1, 6 + 1):
+        vv += dfs(n+1, i)
+    vv /= 6
+
+    return vv if vv > v else v
+
+ans = dfs(0, 0)
+print(ans)
 
