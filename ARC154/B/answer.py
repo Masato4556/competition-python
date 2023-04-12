042ade49d43fcc11703368adf61141f9cd2d32e1
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-04-12 14:39:36 +0900

    ABC154

--- ARC154/B/answer.py
+++ ARC154/B/answer.py
@@ -1,6 +1,21 @@
+from collections import defaultdict
 
-n = int(input())
-m = int(input())
-print(n)
-print(m)
+N = int(input())
+S = input()
+T = input()
 
+for s, t in zip(sorted(S), sorted(T)):
+    if s != t:
+        print(-1)
+        exit()
+
+s_i = N-1
+t_i = N-1
+c = 0 # 動かす必要のない文字の数
+while s_i >= 0 and t_i >= 0:
+    if S[s_i] == T[t_i]: 
+        s_i -= 1
+        c += 1
+    t_i -= 1
+
+print(N-c)
