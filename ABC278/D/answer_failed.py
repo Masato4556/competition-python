ca47f1ab0d96392c047dd676637f97f00e85a5a7
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-04-21 14:02:05 +0900

    ABC278D

--- ABC278/D/answer_failed.py
+++ ABC278/D/answer_failed.py
@@ -1,27 +1,26 @@
-from collections import defaultdict
-
 N = int(input())
 A = list(map(int, input().split()))
 
 Q = int(input())
 
-base = -1
-i_adds = defaultdict(int)
-
+i_adds = [0] * N
+reset = False
+base = 0
 for _ in range(Q):
     query = list(input().split())
+    
     if query[0] == "1":
         x = int(query[1])
+        reset = True
         base = x
-        # base = int(query[1])
-        i_adds = defaultdict(int)
+        i_adds = [0] * N
     elif query[0] == "2":
         i = int(query[1]) - 1
         x = int(query[2])
         i_adds[i] += x
     else: 
         i = int(query[1]) - 1
-        if base != -1:
+        if reset:
             print(base + i_adds[i])
         else:
             print(A[i] + i_adds[i])
--- ABC278/D/lab.py
+++ ABC278/D/lab.py
@@ -1,1 +1,21 @@
 # テストデータ生成など、回答とは関係のないコードを実行するファイル
+import time
+
+
+for n in (1, 10, 100, 1000, 10000, 100000):
+
+    result = 0
+    for _ in range(100):
+        start = time.perf_counter()
+
+
+        A = [0] * n
+        A[-1] = 1
+
+        end = time.perf_counter()
+        run_time = end - start
+        result += run_time
+        # print("実行時間: {}秒".format(run_time))
+
+    print(n)
+    print(result / 100)
