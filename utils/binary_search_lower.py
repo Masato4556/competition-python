5b8b0075ec349e84dd6e053a96578fbd7b0d183a
Otsuka Masato <ootsukamase@gmail.com>
2023-09-10 01:02:09 +0900

    a

--- utils/binary_search_lower.py
+++ utils/binary_search_lower.py
@@ -1,15 +1,7 @@
 # 条件を満たす最も小さい値を見つける
-
 # f: 条件を表す関数。(注意: x未満だったらFalse, x以上だったらTrueになって入る必要がある)
 # lo: 探索範囲の下限
 # hi: 探索範囲の上限
-
-# 説明
-# ・ leftは常に条件を満たさな
-# ・ rightは常に条件を満たす
-# ・ right-left = 1 になるまで範囲を狭める
-# ・ right-left = 1 になった際に、rightが条件を満たす最も小さい値になる
-
 def binary_search_lower(f, lo, hi):
     left = lo
     right = hi
@@ -24,8 +16,14 @@ def binary_search_lower(f, lo, hi):
             right = mid
     return right
 
+# 説明
+# ・ leftは常に条件を満たさな
+# ・ rightは常に条件を満たす
+# ・ right-left = 1 になるまで範囲を狭める
+# ・ right-left = 1 になった際に、rightが条件を満たす最も小さい値になる
 
 # 例: 2乗したら120以上になる最も小さい整数を求める
+
 
 def f(x):
     # 2乗して120を超えたらTrueを返す関数
