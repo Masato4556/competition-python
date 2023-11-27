aed010e0db0d3fe9cae9e705603b3d1301b8b93a
OtsukaMasato <masato-otsuka@cybozu.co.jp>
2023-11-27 13:14:02 +0900

    ABC324D

---
+++ competition/ABC324/D/lab.py
@@ -0,0 +1,18 @@
+# テストデータ生成など、回答とは関係のないコードを実行するファイル
+
+from math import floor 
+
+res = set()
+
+print(floor(pow(10**13, 0.5)))
+
+
+
+
+for i in range(3162278):
+    str()
+    if i == -1:
+        print("a")
+
+#     print(i**2)
+
---
+++ competition/ABC324/F/(WIP)get_testcases.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+from urllib import request
+import datetime
+import argparse
+
+
+def get_task_links(contest_name):
+
+    tasks_url = "https://atcoder.jp/contests/{}/tasks".format(
+        contest_name.lower())
+
+    response = request.urlopen(tasks_url)
+    soup = BeautifulSoup(response, 'html.parser')
+    response.close()
+
+    task_link_elems = soup.select("tr td a")
+    task_links = list(set([elem.get('href') for elem in task_link_elems]))
+    task_links.sort()
+
+    return task_links
+
+
+def get_task_names(task_links):
+    return [task_link.split("/")[-1].upper() for task_link in task_links]
+
+
+def get_testcases(url):
+    response = request.urlopen(url)
+    soup = BeautifulSoup(response, 'html.parser')
+    response.close()
+
+    # テストケースの入力と出力のテキストを取得
+    h3_elems = soup.select("h3")
+    h3_input_testcase_elems = [elem for elem in h3_elems if "入力例" in elem.text]
+    h3_output_testcase_elems = [
+        elem for elem in h3_elems if "出力例" in elem.text]
+
+    input_cases = [elem.next_sibling.text for elem in h3_input_testcase_elems]
+    output_cases = [
+        elem.next_sibling.text for elem in h3_output_testcase_elems]
+
+    return (input_cases, output_cases)
+
+
+parser = argparse.ArgumentParser(description='フォルダの作成')
+dt_now = datetime.datetime.now()
+parser.add_argument('-c', '--contest', default=dt_now.strftime('%Y%m%d%H%M%S'))
+parser.add_argument('-q', '--questions', nargs='*',
+                    default=["A", "B", "C", "D", "E", "F", "G", "H-Ex"])
+parser.add_argument('-g', '--get-testcase', action='store_true')
+
+args = parser.parse_args()
+
+
+task_links = get_task_links(args.contest)
+task_names = get_task_names(task_links)
+
+for i in range(len(task_links)):
+    task_link = task_links[i]
+    task_name = task_names[i]
+    input_cases, output_cases = get_testcases("https://atcoder.jp" + task_link)
+    print(input_cases, output_cases)
+
+    # テストケースのテキストファイルを作成
+    testcase_num = len(input_cases)
+    for i in range(testcase_num):
+        with open("./{}/{}/testcases/input/{}.txt".format(args.contest, task_name, i+1), mode='w') as f:
+            f.write(input_cases[i])
+        with open("./{}/{}/testcases/output/{}.txt".format(args.contest, task_name, i+1), mode='w') as f:
+            f.write(output_cases[i])
