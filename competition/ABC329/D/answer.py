0c99b3d8aad5db416b57e6735804f7db7b183804
Otsuka Masato <ootsukamase@gmail.com>
2024-02-16 09:40:53 +0900

    ABC329DE

--- competition/ABC329/D/answer.py
+++ competition/ABC329/D/answer.py
@@ -1,4 +1,18 @@
+from collections import defaultdict
 
-N = int(input())
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
+
+counter = defaultdict(int)
+
+ans = 10**12
+
+for a in A:
+    counter[a] += 1
+
+    if counter[a] > counter[ans]:
+        ans = a
+    elif counter[a] == counter[ans] and a < ans:
+        ans = a
+
+    print(ans)
--- competition/ABC329/E/answer.py
+++ competition/ABC329/E/answer.py
@@ -1,4 +1,42 @@
+from collections import deque
 
-N = int(input())
 N, M = map(int, input().split())
-A = list(map(int, input().split()))
+S = list(input())
+T = input()
+
+
+def canErase(i, S, T):
+    N = len(S)
+    M = len(T)
+    erased_num = 0
+    if i+M > N:
+        return False
+
+    for j in range(M):
+        if S[i+j] == T[j]:
+            erased_num += 1
+            continue
+        if S[i+j] == "#":
+            continue
+        return False
+
+    return True if erased_num > 0 else False
+
+
+que = deque([])
+
+for i in range(N-M+1):
+    if canErase(i, S, T):
+        que.append(i)
+
+while len(que):
+    i = que.popleft()
+    for j in range(M):
+        S[i+j] = "#"
+
+    for k in range(max(0, i-M+1), min(N, i+M)):
+
+        if canErase(k, S, T):
+            que.append(k)
+
+print("Yes" if S.count('#') == N else "No")
