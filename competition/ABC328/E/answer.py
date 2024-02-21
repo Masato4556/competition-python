c9efa0c6b671dcbd3c089eadc3c26aa5d223f86f
Otsuka Masato <ootsukamase@gmail.com>
2024-02-21 10:59:08 +0900

    ABC328E

--- competition/ABC328/E/answer.py
+++ competition/ABC328/E/answer.py
@@ -20,6 +20,87 @@ def GRID(n): return [input() for _ in range(n)]
 def ZIP(n, func=f): return zip(*(MAP(func) for _ in range(n)))
 
 
-N = INT()
-N, M = MAP()
-A = LIST()
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.par = [-1] * n
+        self.rank = [0] * n
+        self.siz = [1] * n
+
+    def root(self, x):
+        if self.par[x] == -1:
+            return x
+        else:
+            self.par[x] = self.root(self.par[x])  # 経路圧縮
+            return self.par[x]
+
+    def issame(self, x, y):
+        return self.root(x) == self.root(y)
+
+    def unite(self, x, y):
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:
+            return False
+        # union by rank
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.par[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        self.siz[rx] += self.siz[ry]
+        return True
+
+    def size(self, x):
+        return self.siz[self.root(x)]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.par) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):  # Debug用メソッド　計算量多いので使わないように
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.root(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+
+N, M, K = MAP(int)
+
+edges = []
+for _ in range(M):
+    u, v, w = MAP(int)
+    u -= 1
+    v -= 1
+    edges.append((u, v, w))
+
+
+def check(inds):
+    uf = UnionFind(N)
+    cost = 0
+    is_valid = True
+
+    for i in inds:
+        if uf.unite(edges[i][0], edges[i][1]):
+            cost += edges[i][2]
+            cost %= K
+        else:
+            is_valid = False
+            break
+    return (is_valid, cost)
+
+
+ans = float('inf')
+
+inds = list(range(M))
+for selected_inds in combinations(inds, N-1):
+    is_valid, cost = check(selected_inds)
+    if is_valid and cost < ans:
+        ans = cost
+
+print(ans)
